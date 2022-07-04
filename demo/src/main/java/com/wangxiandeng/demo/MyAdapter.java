7565b902ece870ce83e38418d3bf64c95c0b178f
Jeff juhui-pc <330132662@qq.com>
2022-07-04 10:05:05 +0800

    迁移到androidx   ，并升级相关代码、配置

--- demo/src/main/java/com/wangxiandeng/demo/MyAdapter.java
+++ demo/src/main/java/com/wangxiandeng/demo/MyAdapter.java
@@ -1,11 +1,12 @@
 package com.wangxiandeng.demo;
 
 import android.content.Context;
-import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.wangxiandeng.swipecardrecyclerview.SwipeCardAdapter;
 
--- swipecardrecyclerview/src/main/java/com/wangxiandeng/swipecardrecyclerview/SwipeCardAdapter.java
+++ swipecardrecyclerview/src/main/java/com/wangxiandeng/swipecardrecyclerview/SwipeCardAdapter.java
@@ -1,6 +1,6 @@
 package com.wangxiandeng.swipecardrecyclerview;
 
-import android.support.v7.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
